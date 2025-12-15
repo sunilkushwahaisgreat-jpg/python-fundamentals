ea238d163bfcb3878b42f6572c9db5ccf5177691
Sunil Kushwaha <sunilkushwahaisgreat@gmail.com>
2025-12-15 18:42:01 +0530

    Add dictionary practice examples

---
+++ basics/dictionaries.py
@@ -0,0 +1,11 @@
+this_dict={"Name":"Sunil",
+           "Age":22,
+           "College":"NIT Silchar"}
+
+print(this_dict)
+
+print(this_dict.keys())
+print(this_dict.values())
+
+for key,value in this_dict.items():
+    print(key,":",value)
